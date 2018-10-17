755ce4e214e798c20ee1666d2bb6d5e45a9d2f7a
anoble25 <AWGR-Alex@comcast.net>
2018-10-17 13:36:13 -0400

    improve links add active navigation styles

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
     <header>
       <nav>
         <ul>
-          <li class="nav-time">
-            <img class="logo" src="./assets/build-a-bot-logo.png"/>
-            Build-a-Bot
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Home'}" exact>
+              <img class="logo" src="./assets/build-a-bot-logo.png"/>
+              Build-a-Bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'Build'}" exact>
+              Build
+            </router-link>
           </li>
         </ul>
       </nav>
@@ -67,5 +74,12 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active {
+  color: white;
 }
 </style>
